e5149ae6af340112bc42a14224c7ba86a013280f
Tait Hoyem <tait@tait.tech>
2022-04-09 03:47:41 -0600

    Remove some code which caused it to not build

--- input/src/lib.rs
+++ input/src/lib.rs
@@ -1,8 +1,8 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
+// a module to handle input events
+
+// return a channel with inputs from the unix socket
+/*
+pub fn event_channel<T, E>() -> Result<T, E> {
+  
 }
+*/
