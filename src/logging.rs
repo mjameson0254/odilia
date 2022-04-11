9269e40666d118c2822f6e419d1117c3f502229a
alberto tirla <albertotirla@gmail.com>
2022-04-11 17:17:26 +0000

    add formatting to logging in the console with the tracing-tree crate

--- src/logging.rs
+++ src/logging.rs
@@ -3,9 +3,18 @@
 //! Not much here yet, but this will get more complex if we decide to add other layers for error
 //! reporting, tokio-console, etc.
 
+use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+use tracing_tree::HierarchicalLayer;
 /// Initialise the logging stack. Right now this just sets the RUST_LOG environment variable based on the current build(debug or release), then calls [`tracing_subscriber::fmt::init`].
 pub fn init() {
     #[cfg(debug_assertions)]
     std::env::set_var("RUST_LOG", "debug");
-    tracing_subscriber::fmt::init();
+    Registry::default()
+        .with(EnvFilter::from_default_env())
+        .with(
+            HierarchicalLayer::new(4)
+                .with_targets(true)
+                .with_bracketed_fields(true),
+        )
+        .init();
 }
