f8f97cd020bd7ea9cc4dcf9c0806eadcdfe3419c
alberto tirla <albertotirla@gmail.com>
2022-04-12 09:01:18 +0000

    begin refactoring odilia so that it is composed of a binary and library crate, to be further divised into modules

---
+++ src/bin/main.rs
@@ -0,0 +1,15 @@
+use std::sync::Arc;
+
+use color_eyre::eyre::{Result};
+use odilia::*;
+#[tokio::main]
+async fn main() -> Result<()> {
+    setup();
+    let (rproxy, bproxy)=init_accessibility().await?;
+    let rproxy=Arc::new(rproxy);
+    let bproxy=Arc::new(bproxy);
+    register_events(Arc::clone(&rproxy)).await?;
+    spawn_event_tasks(Arc::clone(&rproxy), Arc::clone(&bproxy)).await?;
+    cleanup();
+    Ok(())
+}
