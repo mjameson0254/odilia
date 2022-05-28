ee92d49cc908ed30edb3f5952469f9c0b8d1ab66
alberto tirla <albertotirla@gmail.com>
2022-05-28 16:02:20 +0000

    add instrumentation
    logging is very important to an application as big as odilia, so this commit adds tracing spans and messages to the following methods:
    * new: logs the name parameter, also when initialisation begins and the fact it finished without errors
    * speak: level trace, logs the message that's to be spoken and message id upon completion without errors, useful for looking up problematic messages by the ID in message history
    * drop: just specifies that the speaker object is about to be dropped, useful for trubbleshooting bugs related to resource acquisition and improper resource cleanup, either at exit or otherwise throughout the application

--- tts/src/speaker.rs
+++ tts/src/speaker.rs
@@ -3,32 +3,42 @@ use ssip_client::{
     fifo::{self, Builder},
     ClientError, ClientName,
 };
+use tracing::{debug, trace, warn};
 
 pub struct Speaker {
     client: Client<fifo::UnixStream>,
 }
 impl Speaker {
     ///initialises a `Speaker` object
+    #[tracing::instrument]
     pub fn new(name: &str) -> Result<Self, ClientError> {
+        debug!("initialising speech dispatcher client");
         let mut client = Builder::new().build()?;
+        debug!(name=%name, "setting name and user");
         client
             .set_client_name(ClientName::new(name, name))?
             .check_client_name_set()?;
+        debug!("ssip client initialisation successfull");
         Ok(Self { client })
     }
     ///speaks the given message
+    #[tracing::instrument(level = "trace", skip(self))]
     pub fn speak(&mut self, message: &str) -> Result<(), ClientError> {
-        let _msg_id = self
+        trace!(message=%message, "speaking message");
+        let msg_id = self
             .client
             .speak()?
             .send_line(message)?
             .receive_message_id()?;
+        trace!(id=%msg_id, "speech started successfully");
         Ok(())
     }
 }
 
 impl Drop for Speaker {
+    #[tracing::instrument(skip(self))]
     fn drop(&mut self) {
+        debug!("destroying connection to speech dispatcher socket");
         self.client.quit().unwrap();
     }
 }
