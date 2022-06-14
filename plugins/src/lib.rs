9163d56b09afaf5f392e105e728433813b6fb1b3
Michael Connor Buchan <mikey@lowerelements.club>
2022-06-14 15:39:51 +0100

    Plugins: Implement spawning plugins
    Plugins receive a socket as stdin / stdout that they will soon be able
    to use to communicate with the main Odilia process.

--- plugins/src/lib.rs
+++ plugins/src/lib.rs
@@ -1,1 +1,29 @@
 pub mod config;
+pub use config::Plugin;
+
+use std::{
+    io,
+    os::unix::io::{AsRawFd, FromRawFd},
+    process::Stdio,
+};
+
+use tokio::net::UnixStream;
+
+pub fn spawn(plugin: Plugin) -> io::Result<()> {
+    let (a, b) = UnixStream::pair()?;
+    let b = b.as_raw_fd();
+
+    let mut cmd = plugin.into_command();
+    // Safety: This is always a valid file descriptor
+    cmd.stdin(unsafe { Stdio::from_raw_fd(b) })
+        .stdout(unsafe { Stdio::from_raw_fd(b) });
+
+    let mut child = cmd.spawn()?;
+    tokio::spawn(async move {
+        match child.wait().await {
+            Ok(status) => tracing::info!(%status, "Plugin exited"),
+            Err(e) => tracing::error!(error = %e, "Failed running plugin"),
+        }
+    });
+    Ok(())
+}
